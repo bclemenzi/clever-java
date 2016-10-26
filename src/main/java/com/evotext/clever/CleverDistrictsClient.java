6e6274e174ad5fea6f5b494c99ee9e20e01c32e1
Derek Jackson <derek.jackson@evotext.com>
2016-10-26 14:38:31 -0400

    getAllDistrictStudents fixed

--- src/main/java/com/evotext/clever/CleverDistrictsClient.java
+++ src/main/java/com/evotext/clever/CleverDistrictsClient.java
@@ -362,8 +362,7 @@ public class CleverDistrictsClient extends CleverClient
             JSONObject responseJSON = get(uri, this.m_districtOAuthToken, parameters);
             ObjectMapper mapper = new ObjectMapper();
 
-            Data data = mapper.readValue(responseJSON.toString(), Data.class);
-            List<Link> linkList = data.getLinks();
+            List<Link> linkList = this.getLinkList(responseJSON);
             for (Link link : linkList)
             {
                 if (StringUtils.equals(link.getRel(), "next"))
@@ -484,7 +483,6 @@ public class CleverDistrictsClient extends CleverClient
             JSONObject responseJSON = get(uri, this.m_districtOAuthToken, parameters);
             ObjectMapper mapper = new ObjectMapper();
 
-            //Data data = mapper.readValue(responseJSON.toString(), Data.class);
             List<Link> linkList = this.getLinkList(responseJSON);
             for (Link link : linkList)
             {
